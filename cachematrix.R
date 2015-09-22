ffadb8267f6571504b18d35a7d4c27a80e43b39c
jgervas <jgervas@gmail.com>
2015-09-22 14:32:14 -0300

    Edit Files
    Editing solution for Programming Assignment 2 - R Programming Language

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+ #########################################################################
+  # Function makeCacheMatrix - Create Cacheable vector
+  # Author: Jorge Gervasio Pereira
+  # Date  : Sept, 22 th, 2015
+  
 makeCacheMatrix <- function(x = matrix()) {
-
+  mat <- NULL
+  set <- function(y) {
+    x <<- y
+    mat <<- NULL
+  }
+  
+  get <- function() x
+  setinv <- function(solve) mat <<- solve
+  getinv <- function() mat
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+ #########################################################################
+  # Function cacheSolve - Perform Inverse of Cacheable Matrix, only if Matrix
+  # not already processed
+  # Author: Jorge Gervasio Pereira
+  # Date  : Sept, 22th, 2015
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+mat <- x$getinv()
+  
+  if(!is.null(mat)) {
+    message("getting cached data")
+    return(mat)
+  }
+  
+  data <- x$get()
+  mat <- solve(data)
+  
+  x$setinv(mat)
+  mat
 }
